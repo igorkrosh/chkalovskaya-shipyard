1e9bade3ecb135bcd21d05c4d9857a5c6561c4c5
Igor Kroshkin <igor.krosh@gmail.com>
2021-01-13 23:59:28 +0300

    Секция с видеообращением - Десктоп

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,7 @@ $(document).ready(Core);
 function Core()
 {
     InitWow();
+    InitPlyr();
 }
 
 function InitWow()
@@ -11,4 +12,18 @@ function InitWow()
         animateClass: 'animate__animated',
     })
     wow.init();
+}
+
+function InitPlyr()
+{
+    const player = new Plyr('#player', {
+        controls: ['play-large', 'restart', 'play', 'progress', 'current-time', 'mute', 'volume', 'captions']
+    });
+
+    player.on('playing', event => {
+        if ($('.sect_video .video_wrapper').hasClass('hide_controls'))
+        {
+            $('.sect_video .video_wrapper').removeClass('hide_controls');
+        }
+    })
 }
